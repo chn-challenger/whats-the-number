51487b407c2144e607b966086384eace6293ed48
chn.challenger <chn.challenger@gmail.com>
2015-09-11 13:43:53 +0100

    answers

--- lib/whats_the_number.rb
+++ lib/whats_the_number.rb
@@ -1,6 +1,15 @@
 class WhatsTheNumber
   
-  def self.sum_primes_less_than number
+  def is_prime?(n)
+    n < 2 ? false: (2..Math.sqrt(n)).none? {|x| n % x == 0}
+  end
+
+  def sum_primes_less_than number
+    array = (1..number)
+    array_p = array.select{|p| is_prime?(p)}
+    array_p.inject(0){|result, x| result + x}
   end
 
 end
+
+
